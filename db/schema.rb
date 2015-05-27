ba7c135404b84766f2fb64e4ea5705cd1e62ac4f
Warren Uhrich <warren@agilestyle.com>
2015-05-27 16:21:33 -0600

    Minor update

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150527174923) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "item"
     t.datetime "created_at", null: false
